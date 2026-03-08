19f91b56fba8b482b231e231a38f4d7e732e49cc
musfik070-gif <musfik070@gmail.com>
2026-03-09 03:00:04 +0600

    dashboard page, fixed icon and font isuue

--- login.js
+++ login.js
@@ -1,25 +1,25 @@
-// Login form
+//Login form
 const loginForm = document.getElementById("loginForm");
 
-// Handle form submission
+//Handle form submission
 loginForm.addEventListener("submit", function (event) {
-  // Prevent reload
-  event.preventDefault();
+// Prevent reload
+event.preventDefault();
 
-  // Read inputs
+//Read inputs
   const username = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value.trim();
 
-  //  credentials
+//credentials
   const defaultUsername = "admin";
   const defaultPassword = "admin123";
 
-  // Validate login
+//Validate login
   if (username === defaultUsername && password === defaultPassword) {
-    // Store login flag
+//Store login flag
     localStorage.setItem("isLoggedIn", "true");
 
-    // Go to dashboard
+//Go to dashboard
     window.location.href = "dashboard.html";
   } else {
     alert("Invalid username or password");
